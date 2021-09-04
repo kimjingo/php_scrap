53bdc03da296bafb55b35d43bc300fb1b1e77b79
Jingoo Kim <kimjingo@gmail.com>
2021-09-03 23:14:31 -0700

    created crawler

--- scrap_ktv_multi.php
+++ scrap_ktv_multi.php
@@ -19,20 +19,58 @@ if ($con->query($sql) === TRUE) {
     echo "Error creating table: " . $con->error;
 }
 
+function crawl_page($url, $depth = 5)
+{
+    static $seen = array();
+    if (isset($seen[$url]) || $depth === 0) {
+        return;
+    }
+
+    $seen[$url] = true;
+
+    $dom = new DOMDocument('1.0');
+    @$dom->loadHTMLFile($url);
+
+    $anchors = $dom->getElementsByTagName('a');
+    foreach ($anchors as $element) {
+        $href = $element->getAttribute('href');
+        if (0 !== strpos($href, 'http')) {
+            $path = '/' . ltrim($href, '/');
+            if (extension_loaded('http')) {
+                $href = http_build_url($url, array('path' => $path));
+            } else {
+                $parts = parse_url($url);
+                $href = $parts['scheme'] . '://';
+                if (isset($parts['user']) && isset($parts['pass'])) {
+                    $href .= $parts['user'] . ':' . $parts['pass'] . '@';
+                }
+                $href .= $parts['host'];
+                if (isset($parts['port'])) {
+                    $href .= ':' . $parts['port'];
+                }
+                $href .= dirname($parts['path'], 1).$path;
+            }
+        }
+        crawl_page($href, $depth - 1);
+    }
+    echo "URL:",$url,PHP_EOL,"CONTENT:",PHP_EOL,$dom->saveHTML(),PHP_EOL,PHP_EOL;
+}
+crawl_page("http://kfoodot.com", 2);
+
 // $ip_addr = $_SERVER['REMOTE_ADDR'] . (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])? ' from '.$_SERVER['HTTP_X_FORWARDED_FOR']:'');
 
-$curr = time();
-$qry = "SELECT * FROM {$table} ORDER BY id DESC LIMIT 1";
-if($QR = $con->query($qry)){
-  $QD = $QR->fetch_assoc();
-  if( ($QD['created_at'] > ($curr-50) ) && ($QD['ip_addr'] == $ip_addr) ){
-    $message = "Please try again later";
-    // require('contact.php');
-    exit;
-  }
-} else {
-  echo "Error: " . $qry . $con->error;
-}
+// $curr = time();
+// $qry = "SELECT * FROM {$table} ORDER BY id DESC LIMIT 1";
+// if($QR = $con->query($qry)){
+//   $QD = $QR->fetch_assoc();
+//   if( ($QD['created_at'] > ($curr-50) ) && ($QD['ip_addr'] == $ip_addr) ){
+//     $message = "Please try again later";
+//     // require('contact.php');
+//     exit;
+//   }
+// } else {
+//   echo "Error: " . $qry . $con->error;
+// }
 
 // $stmt = $con->prepare("INSERT INTO contacts(first_name, last_name, email, division, subject, message, ip_addr) VALUES (?, ?, ?, ?, ?, ?, ?)");
 // $stmt->bind_param('sssssss', $first_name, $last_name, $email, $division, $subject, $message, $ip_addr);
